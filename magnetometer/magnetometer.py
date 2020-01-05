74afe830c6002bab86ab6ed6fcc9bb3b225e66cd
MatsAndT <matsandreas@tonnesland.name>
2020-01-05 21:37:20 +0100

    getCompass in MagneticField

--- magnetometer/magnetometer.py
+++ magnetometer/magnetometer.py
@@ -3,3 +3,8 @@ from sense_hat import SenseHat
 class MagneticField():
     def __init__(self):
         self.sense = SenseHat()
+
+    def getCompass(self):
+        compass = self.sense.get_compass_raw()
+
+        return [compass['z'], compass['y'], compass['x']]
