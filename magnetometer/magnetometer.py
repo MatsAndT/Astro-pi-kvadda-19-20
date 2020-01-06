57b30094f0939eb913f5c1ac17b89b1c22ee736e
Mats Andreas Tønnesland <matsandreas@tonnesland.name>
2020-01-06 09:36:45 +0100

    info in getCompass

--- magnetometer/magnetometer.py
+++ magnetometer/magnetometer.py
@@ -5,6 +5,8 @@ class MagneticField():
         self.sense = SenseHat()
 
     def getCompass(self):
+        # Getting the raw compass data
         compass = self.sense.get_compass_raw()
 
+        # Return the comass rew compass data from z, y and x in uT (micro teslas)
         return [compass['z'], compass['y'], compass['x']]
