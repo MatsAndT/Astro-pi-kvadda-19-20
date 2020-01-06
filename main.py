ca83affb01ec54ef378066118087515e3ada747a
MatsAndT <matsandreas@tonnesland.name>
2020-01-06 21:53:07 +0100

    db init

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ from camera.camera import capture_image
 data_manager = DataManager()
 magnetic_field = MagneticField()
 
+conn = data_manager.create_connection()
+data_manager.create_table(conn) # TODO: if fasle (error) return
