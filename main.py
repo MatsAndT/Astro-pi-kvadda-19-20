071ac902ce358256d22d143aa8a09601e3303d7a
MatsAndT <matsandreas@tonnesland.name>
2020-01-06 21:53:16 +0100

    main while

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ magnetic_field = MagneticField()
 
 conn = data_manager.create_connection()
 data_manager.create_table(conn) # TODO: if fasle (error) return
+
+while True:
+    frame = capture_image()
+    magnetic_field_raw = magnetic_field.getCompass()
+
+    data_manager.insert_data(conn,frame,magnetic_field_raw[0],magnetic_field_raw[1],magnetic_field_raw[2])
+    
