803d21571f6739f2ba0b47d2b58f4c2ea4c566c1
MatsAndT <matsandreas@tonnesland.name>
2020-01-06 21:52:55 +0100

    set class data and magnetic

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ from data_manager.data_manager import DataManager
 from magnetometer.magnetometer import MagneticField
 from camera.camera import capture_image
 
+data_manager = DataManager()
+magnetic_field = MagneticField()
+
