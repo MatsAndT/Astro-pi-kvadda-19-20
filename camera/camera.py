0ea345e6c3dbba39da1a88283f0d6f6ec5e4a71a
Pyotr <36033037+pYtato-KK@users.noreply.github.com>
2020-01-14 22:57:31 +0100

    Switch to picamera, which seems more stable.

--- camera/camera.py
+++ camera/camera.py
@@ -1,14 +1,12 @@
-import time
+from picamera import PiCamera
+from time import sleep
 import cv2
-import atexit
 
-capture = cv2.VideoCapture(0)
-capture.set(3, 2592)
-capture.set(4, 1944)
-capture.set(5, 10)
-atexit.register(capture.release)
-time.sleep(1) # Camera warmup
+path = "./tmp/lastimage.jpg"
+
+camera = PiCamera()
+sleep(2)
 
 def capture_image():
-    _, frame = capture.read()
-    return frame
+    camera.capture(path)
+    return cv2.imread(path)
