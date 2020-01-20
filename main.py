f2c25e1b70a51e45a28e36546717322819c2081b
Mats Andreas Tønnesland <matsandreas@tonnesland.name>
2020-01-20 11:00:44 +0100

    main class

--- main.py
+++ main.py
@@ -5,12 +5,5 @@ from camera.camera import  Camera
 data_manager = DataManager()
 magnetic_field = MagneticField()
 
-conn = data_manager.create_connection()
-data_manager.create_table(conn) # TODO: if fasle (error) return
-
-while True:
-    frame = capture_image()
-    magnetic_field_raw = magnetic_field.getCompass()
-
-    data_manager.insert_data(conn,frame,magnetic_field_raw[0],magnetic_field_raw[1],magnetic_field_raw[2])
-    
+class Main():
+    pass
