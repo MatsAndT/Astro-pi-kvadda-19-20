7a7f1b15eb66c05441d1d0983fbdd79533061f8f
Pyotr <36033037+pYtato-KK@users.noreply.github.com>
2020-01-26 17:13:18 +0100

    Fix syntax error and fix unreachable code

--- camera/camera.py
+++ camera/camera.py
@@ -4,9 +4,10 @@ import cv2
 
 class Camera(PiCamera):
     def __init__(self, *args, **kwargs):
-        self.path = kwargs.pop("path", ""./tmp/lastimage.jpg"")
-        return super().__init__(*args, **kwargs)
-        sleep(2)
+        self.path = kwargs.pop("path", "./tmp/lastimage.jpg")
+        s = super().__init__(*args, **kwargs)
+        sleep(2) # Pause to give the camera time to adjust
+        return s
 
     def capture_image(self):
         camera.capture(self.path)
