3741c83448bcc1a6839c85d02bc62c85cfd0c635
Pyotr <36033037+pYtato-KK@users.noreply.github.com>
2020-01-26 17:06:04 +0100

    More descriptive variable names

--- image/image.py
+++ image/image.py
@@ -7,7 +7,7 @@ class Image:
         # Check if image is numpy.ndarray
         if not isinstance(image, numpy.ndarray):
             raise TypeError('Expected image, not {}'.format(type(image)))
-        
+
         self.original = image
 
     @property
@@ -17,12 +17,12 @@ class Image:
         :return: numpy.ndarray NDVI image
         """
         # Get each color of the image
-        b, _, nir = cv2.split(self.original) 
+        blue, _, near_ir = cv2.split(self.original) 
 
-        bottom = nir.astype(float) + b.astype(float)
+        bottom = near_ir.astype(float) + blue.astype(float)
         bottom[bottom == 0] = 0.0000000000000001 # Replace zeroes with near zero value
 
-        top = nir.astype(float) - b
+        top = near_ir.astype(float) - blue.astype(float)
         
         return top / bottom
 
