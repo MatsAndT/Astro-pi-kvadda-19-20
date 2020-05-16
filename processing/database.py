f3f5f6d25776997ea2ea07d742b08e809dd8415c
MatsAndT <matsandreas@tonnesland.name>
2020-05-16 23:17:13 +0200

    started database class

---
+++ processing/database.py
@@ -0,0 +1,22 @@
+import os
+import sqlite3
+from sqlite3 import Error
+
+class DataBase:
+    line = 0
+
+    def __init__(self, db_path):
+        super().__init__()
+
+        try:
+            self.conn = sqlite3.connect(os.path.abspath(self.db_name))
+        except Error as e:
+            raise SyntaxError(e)
+
+    def next(self):
+        pass
+
+    def get_data(self):
+        pass
+
+    
