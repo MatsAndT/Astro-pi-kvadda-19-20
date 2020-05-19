a9aeae591cf2ef65535fab704e2ae717c7fdf39c
Mats Andreas Tønnesland <matsandreas@tonnesland.name>
2020-05-19 18:11:14 +0200

    added loop to main

--- processing/processing.py
+++ processing/processing.py
@@ -1,10 +1,20 @@
 import argparse
+from database import DataBase
+from time_to_position import TimeToLatLon
 
 db_path = '../data/teamkvadda_data_database.sqlite'
 time_zone = '+0000'
 
 def main(folder, db):
-    pass
+    db_handeler = DataBase(db_path)
+    ttp = TimeToLatLon()
+
+    while True:
+        row = db_handeler.next()
+        lat, lon = ttp.convert(row['time']+time_zone)
+
+        address = to_address(lat, lon)
+        town, country = getplace(lat, lon)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process some iss data.')
